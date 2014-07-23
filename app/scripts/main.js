111851a7eecfd4f22754e355ae095a8dd3d064e6
Laura Patti <lpatti10@gmail.com>
2014-07-23 15:52:54 -0400

    Finished setup of forms

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('The Iron Yard Rocks');
+Parse.initialize("aUOgGVzu66uKF45tTRiIidlQJ1J9gfZjRWiNmrJC", "bjOQ1QJn0D2zHoNlDNpp1KaQucgsznkISsEB1aGi");
+
+// TEST FOR PARSE INSTALL
+// var TestObject = Parse.Object.extend("TestObject");
+// var testObject = new TestObject();
+// testObject.save({foo: "bar"}).then(function(object) {
+//   alert("yay! it worked");
+// });
