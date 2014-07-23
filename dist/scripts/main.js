d6fc91c043bdeed848c824a004a5a442324dbdb2
Laura Patti <lpatti10@gmail.com>
2014-07-23 19:41:34 -0400

    repairs for gulp

---
+++ dist/scripts/main.js
@@ -0,0 +1,41 @@
+// NATIVE PARSE BLOCK
+var user = new Parse.User();
+
+user.set("username", "my name");
+user.set("password", "my pass");
+user.set("email", "email@example.com");
+ 
+// other fields can be set just like with Parse.Object
+user.set("phone", "415-392-0202");
+ 
+user.signUp(null, {
+  success: function(user) {
+    // Hooray! Let them use the app now.
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
+
+///////////////////////////////////////////////////////////////////
+
+
+// NATIVE PARSE BLOCK
+Parse.User.logIn("myname", "mypass", {
+  success: function(user) {
+    // Do stuff after successful login.
+  },
+  error: function(user, error) {
+    // The login failed. Check error to see why.
+  }
+});
+Parse.initialize("aUOgGVzu66uKF45tTRiIidlQJ1J9gfZjRWiNmrJC", "bjOQ1QJn0D2zHoNlDNpp1KaQucgsznkISsEB1aGi");
+
+// TEST FOR PARSE INSTALL
+// var TestObject = Parse.Object.extend("TestObject");
+// var testObject = new TestObject();
+// testObject.save({foo: "bar"}).then(function(object) {
+//   alert("yay! it worked");
+// });
+
