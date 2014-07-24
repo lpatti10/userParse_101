de473904e4eba9b93b0db9f033bcc2e856a61dc0
Laura Patti <lpatti10@gmail.com>
2014-07-24 09:37:04 -0400

    Basic user table established

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,42 +8,15 @@ Parse.initialize("aUOgGVzu66uKF45tTRiIidlQJ1J9gfZjRWiNmrJC", "bjOQ1QJn0D2zHoNlDN
 // });
 
 // NATIVE PARSE CODE BLOCK
-// var user = new Parse.User();
-
-// user.set("username", "my name");
-// user.set("password", "my pass");
-// user.set("email", "email@example.com");
- 
-// other fields can be set just like with Parse.Object
-// user.set("phone", "415-392-0202");
- 
-// user.signUp(null, {
-//   success: function(user) {
-//     // Hooray! Let them use the app now.
-//   },
-//   error: function(user, error) {
-//     // Show the error message somewhere and let the user try again.
-//     alert("Error: " + error.code + " " + error.message);
-//   }
-// });
-
-///////////////////////////////////////////////////////////////////
 var user = new Parse.User();
 
-
-
-$(signupForm).on('submit' function (event) {
-	event.preventDefaults();
-
-	var user_name = $(this).find(#username).val();
-	var user_pass = $(this).find(#password).val();
-
-});
-
-
-user.set("username", "user_name");
-user.set("password", "user_pass");
-
+user.set("username", "my name");
+user.set("password", "my pass");
+user.set("email", "email@example.com");
+ 
+// other fields can be set just like with Parse.Object
+user.set("phone", "415-392-0202");
+ 
 user.signUp(null, {
   success: function(user) {
     // Hooray! Let them use the app now.
@@ -51,13 +24,40 @@ user.signUp(null, {
   error: function(user, error) {
     // Show the error message somewhere and let the user try again.
     alert("Error: " + error.code + " " + error.message);
-  },
-
-  $( this ).trigger( 'reset' );
-  $( '#signupForm' ).trigger( 'reset' );
-  $( '#loginForm' ).trigger( 'reset' );
-
+  }
 });
+
+///////////////////////////////////////////////////////////////////
+// var user = new Parse.User();
+
+
+
+// // $(signupForm).on('submit' function (event) {
+// // 	event.preventDefaults();
+
+// // 	var user_name = $(this).find(#username).val();
+// // 	var user_pass = $(this).find(#password).val();
+
+// // });
+
+
+// user.set("username", "user_name");
+// user.set("password", "user_pass");
+
+// user.signUp(null, {
+//   success: function(user) {
+//     // Hooray! Let them use the app now.
+//   },
+//   error: function(user, error) {
+//     // Show the error message somewhere and let the user try again.
+//     alert("Error: " + error.code + " " + error.message);
+//   },
+
+//   $( this ).trigger( 'reset' );
+//   $( '#signupForm' ).trigger( 'reset' );
+//   $( '#loginForm' ).trigger( 'reset' );
+
+// });
 
 
 
